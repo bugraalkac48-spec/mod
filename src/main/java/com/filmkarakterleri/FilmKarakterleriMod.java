a9c82d521b48d866808361202a8c334c372e400b
bugraalkac48-spec <bugraalkac48@gmail.com>
2026-07-14 21:42:27 +0300

    Update FilmKarakterleriMod.java

--- src/main/java/com/filmkarakterleri/FilmKarakterleriMod.java
+++ src/main/java/com/filmkarakterleri/FilmKarakterleriMod.java
@@ -1,25 +1,11 @@
 package com.filmkarakterleri;
 
-import com.filmkarakterleri.entity.ModEntities;
-import com.filmkarakterleri.event.ChatReplikHandler;
-import com.filmkarakterleri.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(FilmKarakterleriMod.MODID)
+@Mod("filmkarakterlerimod")
 public class FilmKarakterleriMod {
-    public static final String MODID = "filmkarakterlerimod";
-
     public FilmKarakterleriMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Kayıt işlemlerini buraya bağladık
-        ModItems.register(modEventBus);
-        ModEntities.register(modEventBus);
-
         MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.register(new ChatReplikHandler());
     }
 }
