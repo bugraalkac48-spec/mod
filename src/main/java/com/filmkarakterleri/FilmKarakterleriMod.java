2aad9028d5a6561f93836b140abc3b4cb475ca19
bugraalkac48-spec <bugraalkac48@gmail.com>
2026-07-14 21:24:28 +0300

    Update FilmKarakterleriMod.java

--- src/main/java/com/filmkarakterleri/FilmKarakterleriMod.java
+++ src/main/java/com/filmkarakterleri/FilmKarakterleriMod.java
@@ -8,28 +8,18 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-/**
- * Film Karakterleri Modu - Ana giriş noktası.
- *
- * Bu mod, 10 farklı "film karakteri" tipinde evcilleştirilebilir bir varlık (entity)
- * ekler. Her karakterin kendi ismi, texture'ı ve chat repliği listesi vardır.
- *
- * NOT: Gerçek karakter isimleri/görselleri/replikleri telif hakkı taşıyabileceğinden
- * bu iskelette yer tutucu (placeholder) değerler kullanılmıştır. Kendi içeriğinle
- * KarakterData.java dosyasını doldurman gerekir.
- */
 @Mod(FilmKarakterleriMod.MODID)
 public class FilmKarakterleriMod {
-
-    public static final String MODID = "filmkarakterleri";
+    public static final String MODID = "filmkarakterlerimod";
 
     public FilmKarakterleriMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ModEntities.ENTITY_TYPES.register(modEventBus);
-        ModItems.ITEMS.register(modEventBus);
+        // Kayıt işlemlerini buraya bağladık
+        ModItems.register(modEventBus);
+        ModEntities.register(modEventBus);
 
-        // Chat üzerinden repliklerin tetiklenmesi için event handler'ı kaydet
+        MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new ChatReplikHandler());
     }
 }
